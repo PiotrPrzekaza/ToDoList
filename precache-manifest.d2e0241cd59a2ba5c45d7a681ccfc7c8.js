f43c55a5230ebc272a13c7effde35ce13686495c
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-04-15 19:13:45 +0200

    Updates

---
+++ precache-manifest.d2e0241cd59a2ba5c45d7a681ccfc7c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a71c43a298729cf07dd",
+    "url": "/ToDoList/static/js/runtime~main.67fadf76.js"
+  },
+  {
+    "revision": "a3c48128b1b9c57d006d",
+    "url": "/ToDoList/static/js/main.1b62ed3f.chunk.js"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/ToDoList/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "a3c48128b1b9c57d006d",
+    "url": "/ToDoList/static/css/main.836a7f93.chunk.css"
+  },
+  {
+    "revision": "8d5bf33db3da4d314079a303aba9d125",
+    "url": "/ToDoList/index.html"
+  }
+];
